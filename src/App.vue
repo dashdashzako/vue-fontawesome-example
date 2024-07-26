7a06ab0986f8eff8527af05ed12debbdb977ed68
Damien Caselli <damien.caselli@gmail.com>
2024-07-26 16:09:35 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faHandshake } from '@fortawesome/free-regular-svg-icons'
+</script>
+
+<template>
+  <main>
+    Hello
+    <FontAwesomeIcon :icon="faHandshake" />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
